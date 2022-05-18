d23851fb3e654c87a309f7d034e9d82fcda69349
LuizPaixao_rp <luiz.paixao@conecta.tech>
2022-05-18 17:54:31 -0300

    .

--- js/questions.js
+++ js/questions.js
@@ -1,8 +1,9 @@
 // creating an array and passing the number, questions, options, and answers
-let questions = [
+let listQuestions = [
     {
     numb: 1,
     question: "What does HTML stand for?",
+    problem: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     answer: "Hyper Text Markup Language",
     options: [
       "Hyper Text Preprocessor",
@@ -14,6 +15,7 @@ let questions = [
     {
     numb: 2,
     question: "What does CSS stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
     answer: "Cascading Style Sheet",
     options: [
       "Common Style Sheet",
@@ -25,6 +27,7 @@ let questions = [
     {
     numb: 3,
     question: "What does PHP stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
     answer: "Hypertext Preprocessor",
     options: [
       "Hypertext Preprocessor",
@@ -36,6 +39,7 @@ let questions = [
     {
     numb: 4,
     question: "What does SQL stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
     answer: "Structured Query Language",
     options: [
       "Stylish Question Language",
@@ -47,6 +51,7 @@ let questions = [
     {
     numb: 5,
     question: "What does XML stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
     answer: "eXtensible Markup Language",
     options: [
       "eXtensible Markup Language",
@@ -55,18 +60,64 @@ let questions = [
       "eXamine Multiple Language"
     ]
   },
-  // you can uncomment the below codes and make duplicate as more as you want to add question
-  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
-
-  //   {
-  //   numb: 6,
-  //   question: "Your Question is Here",
-  //   answer: "Correct answer of the question is here",
-  //   options: [
-  //     "Option 1",
-  //     "option 2",
-  //     "option 3",
-  //     "option 4"
-  //   ]
-  // },
+  {
+   numb: 6,
+    question: "What does HTML stand for?",
+    problem: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 7,
+    question: "What does CSS stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 8,
+    question: "What does PHP stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 9,
+    question: "What does SQL stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What does XML stand for?",
+    problem: "TESTE TESTE TESTE TESTE",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
 ];
